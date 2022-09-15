eecff465b392eb307108e3f59150e95dca044f2b
Issarapap <film4777@gmail.com>
2022-09-15 10:39:09 +0700

    base button style

---
+++ src/component/Button/BaseButton.tsx
@@ -0,0 +1,21 @@
+import { Button, ButtonProps } from 'antd';
+import styled from "styled-components";
+
+export interface BaseButtonProps extends ButtonProps {
+
+}
+
+const StyledButton = styled(Button)`
+  margin-top:30px;
+  width: 140px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+`;
+const BaseButton = (props: BaseButtonProps) => {
+  return (
+    <StyledButton {...props} />
+  )
+}
+
+export default BaseButton
