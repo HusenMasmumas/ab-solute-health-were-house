89355344727eea4ae2fffcee66ec4c7d63bda625
Issarapap <film4777@gmail.com>
2022-09-26 11:33:17 +0700

    feat: 🎸 createQueryString

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,3 +6,28 @@ export const _findObjectOfArrayByKeyName = (
   const obj = array.find((x) => x[keyName] === value);
   return !!obj ? obj : null;
 };
+
+export const createQueryString = (values: any) => {
+  if (!values) {
+    return "";
+  }
+  let query = "";
+  let result = {};
+  for (const [key, value] of Object.entries(values)) {
+    if (value || value === 0 || value === false) {
+      result = { ...result, [key]: value };
+    }
+  }
+  Object.entries(result).forEach(([key, value], index) => {
+    if (index === Object.keys(result).length - 1) {
+      if (value || value === 0 || value === false) {
+        query += `${key}=${value}`;
+      }
+    } else {
+      if (value || value === 0 || value === false) {
+        query += `${key}=${value}&`;
+      }
+    }
+  });
+  return "?" + query;
+};
