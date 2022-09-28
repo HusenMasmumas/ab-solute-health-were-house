7c4b2379e8d5499b1ecb41b365947a211fdb3d2d
Issarapap <film4777@gmail.com>
2022-09-28 10:33:55 +0700

    refactor: 💡 mock data

--- src/views/purchase_order/manage/SendBack.tsx
+++ src/views/purchase_order/manage/SendBack.tsx
@@ -75,9 +75,44 @@ const SendBack = (props: Props) => {
             </div>
           );
         }}
+        data={mock}
       />
     </div>
   );
 };
 
 export default SendBack;
+
+
+const mock = [
+  {
+    key: 1,
+    date: "23-02-2564",
+    code: "asdsad",
+    branch: "ร้านขายยาวังทองหลาง",
+    fullname: "สมพงษ์ ตามังกร",
+    phone: "0934213455",
+    pay: "3000",
+    status: "ยกเลิก",
+  },
+  {
+    key: 2,
+    date: "23-02-2569",
+    code: "asdsad",
+    branch: "ร้านขายยาวังทองหลาง",
+    fullname: "สมพงษ์ ตามังกร",
+    phone: "0934213455",
+    pay: "3000",
+    status: "อนุมัติ",
+  },
+  {
+    key: 3,
+    date: "24-02-2569",
+    code: "asdsad",
+    branch: "ร้านขายยาวังทองหลาง",
+    fullname: "สมพงษ์ ตามังกร",
+    phone: "0934213455",
+    pay: "3000",
+    status: "อนุมัติ",
+  },
+];
