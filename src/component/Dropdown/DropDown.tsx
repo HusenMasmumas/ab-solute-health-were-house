c5139e71f3724f4295b45bfb2798c1beaad22a65
Issarapap <film4777@gmail.com>
2022-09-29 21:50:36 +0700

    style: 💄 dropdown compopent

---
+++ src/component/Dropdown/DropDown.tsx
@@ -0,0 +1,85 @@
+import { Dropdown, Menu, MenuProps } from 'antd'
+import { DownOutlined } from '@ant-design/icons';
+import styled from "styled-components";
+
+interface Props  {
+    background: string;
+    hoverBackground?: string;
+    selection: ISelectTable;
+}
+
+interface ISelectTable {
+  title: string,
+  option: IOptionTable[]
+}
+
+interface IOptionTable {
+  label: string,
+  value: any,
+  action: (values?: any) => void;
+}
+
+interface IStyleMenu extends MenuProps , Omit<Props, 'selection'> {}
+
+const StyledMenu = styled(Menu)<IStyleMenu>`
+  padding: 0px;
+  background-color: ${({ background }) => background} !important;
+  color: #fff;
+  
+  .ant-dropdown-menu-item {
+    color:#fff !important;
+    &:hover{
+      background-color: ${({ hoverBackground }) => hoverBackground} !important;
+    }
+  }
+`
+
+const StyleDropdown = styled(Dropdown)<{cursor:boolean}>`
+  &:hover{
+    cursor: pointer !important; 
+    cursor: ${({ cursor }) => cursor ? 'pointer' : 'default' } !important; 
+  }
+`
+
+const CDropDown = ({ background, hoverBackground, selection}: Props) => {
+
+    const { option=[] } = selection
+
+    const handleMenuClick = (e:any) => {
+        const find = option.find(item=> item.label === e.key)
+        if(find){
+          find.action(find.value)
+        }
+    }
+
+    const menu = (
+        <StyledMenu
+          background={background}
+          hoverBackground={hoverBackground}
+          onClick={handleMenuClick}
+          items={ option.map((item:IOptionTable) => (
+                {
+                  label:item.label,
+                  key: item.label,
+                }
+          )) } 
+        />
+    );
+
+  return (
+    <StyleDropdown 
+      overlay={menu} 
+      trigger={['click']}
+      cursor={ option.length > 0 ? true : false}
+      >
+        <div className={`flex items-center text-white bg-[${background}] h-[40px] rounded-md px-4 !w-[130px]`}>
+          <div className='!w-[100px] text-center '>
+            {selection.title}
+          </div>
+          { option.length > 0 && <DownOutlined />}
+        </div>
+    </StyleDropdown>
+  )
+}
+
+export default CDropDown
