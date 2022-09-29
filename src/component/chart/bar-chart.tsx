a753d862979f535f3db3e7abfb04b37e740549c1
Prakaifak <prakaifak71@gamil.com>
2022-09-29 18:45:36 +0700

    refactor: 🎨chart

--- src/component/chart/bar-chart.tsx
+++ src/component/chart/bar-chart.tsx
@@ -9,12 +9,12 @@ const dataSourceBar = [
 ];
 const optionsBar = {
   chart: { toolbar: { show: false } },
+  tooltip: { enabled: false },
   plotOptions: {
     bar: {
+      barHeight: "60%",
       horizontal: true,
-      dataLabels: {
-        position: "top",
-      },
+      dataLabels: { position: "top" },
     },
   },
   xaxis: {
@@ -23,6 +23,21 @@ const optionsBar = {
       show: false,
     },
   },
+  yaxis: {
+    labels: {
+      show: true,
+      style: {
+        fontFamily: "SukhumvitSet",
+        fontSize: "14px",
+      },
+      align: "left",
+    },
+    crosshairs: {
+      show: false,
+    },
+    axisBorder: { show: false },
+    axisTicks: { show: false },
+  },
   dataLabels: {
     enabled: true,
     offsetX: 40,
@@ -30,15 +45,14 @@ const optionsBar = {
     style: {
       fontSize: "12px",
       colors: ["#000000"],
+      fontFamily: "SukhumvitSet",
     },
   },
-  tooltip: {
-    enabled: false,
-  },
-  stroke: {
+  stroke: { show: true, width: 10, colors: ["#ffffff"] },
+  grid: {
     show: true,
-    width: 10,
-    colors: ["#fff"],
+    xaxis: { lines: { show: true } },
+    yaxis: { lines: { show: false } },
   },
 } as ApexOptions;
 
@@ -57,3 +71,54 @@ const BarChart = () => {
 };
 
 export default BarChart;
+
+// const options = {
+//   chart: { type: "bar", toolbar: { show: false } },
+//   tooltip: {
+//     shared: true,
+//     intersect: false,
+//     theme: "dark",
+//     enabled: true,
+//     style: { fontSize: "12px", fontFamily: undefined },
+//     onDatasetHover: { highlightDataSeries: false },
+//     y: { formatter: undefined, title: { formatter: (seriesName: any) => "" } },
+//   },
+//   dataLabels: {
+//     style: { fontSize: "16px", fontWeight: "300", colors: ["#000000"] },
+//   },
+//   colors: ["#E2C799"],
+//   plotOptions: {
+//     bar: {
+//       barHeight: "40%",
+//       horizontal: true,
+//       dataLabels: { position: "top" },
+//     },
+//   },
+//   stroke: { show: true, width: 1, colors: ["#E2C799"] },
+//   fill: { opacity: 1 },
+//   xaxis: {
+//     categories: mostStation.map((item: IMostStation) => item?.name),
+//     labels: {
+//       show: false,
+//       style: { colors: ["#FFFFFF"], fontSize: "16px", fontWeight: 200 },
+//     },
+//     axisBorder: { show: false },
+//     axisTicks: { show: false },
+//   },
+//   yaxis: {
+//     labels: {
+//       show: true,
+//       align: "left",
+//       style: {
+//         colors: ["#FFFFFF", "#FFFFFF", "#FFFFFF"],
+//         fontSize: "12px",
+//         fontWeight: 100,
+//       },
+//     },
+//   },
+//   grid: {
+//     show: true,
+//     xaxis: { lines: { show: true } },
+//     yaxis: { lines: { show: false } },
+//   },
+// } as ApexOptions;
