a82c6840da43aa149f6d5eeb20f67690215a0f1d
Issarapap <film4777@gmail.com>
2022-09-30 12:21:53 +0700

    refactor: 💡 required props

--- src/component/Button/TabeButton.tsx
+++ src/component/Button/TabeButton.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import BaseButton, { BaseButtonProps } from "./BaseButton";
 import { useTranslation } from "react-i18next";
-const StyledButton = styled(BaseButton)<{active?:number}>`
+const StyledButton = styled(BaseButton)<{active:number}>`
   color: #949594;
   font-size: 16px;
   height: 50px;
