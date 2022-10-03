d40a364defdac99a8a7e036eef995003e03041a4
Issarapap <film4777@gmail.com>
2022-10-03 12:04:50 +0700

    refactor: 💡 delect file

--- src/page/purchase_order/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useTranslation } from "react-i18next";
-
-const PurchaseOrder = () => {
-  const { t } = useTranslation();
-  return <div>{`${t("purchaseOrder")}`}</div>;
-};
-
-export default PurchaseOrder;
