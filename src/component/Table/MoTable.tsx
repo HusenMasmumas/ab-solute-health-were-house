845649cebe8903de60cff6957062fd9a374488bf
Issarapap <film4777@gmail.com>
2022-10-07 01:56:36 +0700

    style: 💄 header condition code

--- src/component/Table/MoTable.tsx
+++ src/component/Table/MoTable.tsx
@@ -1,17 +1,16 @@
 import { Table, Pagination, TableProps, Card } from "antd";
 
-
-type actionType = 'excel'
+type actionType = "excel";
 interface Props extends TableProps<any> {
   headerTable?: String;
   config?: any;
   onChangePage?: (page: number, pageSize?: string) => void;
-  onClickRow?: (row : any) => void;
+  onClickRow?: (row: any) => void;
   pagination?: boolean | any;
   onDoubleClick?: boolean;
   columns?: any[];
   action?: {
-    action: actionType
+    action: actionType;
     fn: (row?: any) => void;
   }[];
   scroll?: any;
@@ -32,57 +31,42 @@ const MoTable = ({
 }: Props) => {
   return (
     <>
-      <Card 
-      className="w-full !p-0 !my-10"
-      bodyStyle={{paddingTop:0}}
-      >
-      {action ||
-        (headerTable && (
-          <div className="flex h-16">
-            {headerTable ? (
-              <div className="text-[20px] font-semibold w-[70%] flex items-center">
-                {headerTable}
-              </div>
-            ) : (
-              <div className="text-[20px] font-semibold w-[70%] flex items-center">
-                {headerTable}
-              </div>
-            )}
-            {action ? (
-              <div className="ml-8 w-[30%] flex items-center">
-                section action
-              </div>
-            ) : (
-              <div className="ml-8 w-[30%] flex items-center"></div>
-            )}
+      <Card className="w-full !p-0 !my-10" bodyStyle={{ paddingTop: 0 }}>
+        <div className="flex h-16">
+          <div className="text-[20px] font-semibold w-[70%] flex items-center">
+            {headerTable ? headerTable : null}
+          </div> 
+          <div className="ml-8 w-[30%] flex items-center">
+            {action ? 'section action' : 'section action'}
           </div>
-        ))}
-      <Table
-        columns={columns}
-        dataSource={[...dataSource]}
-        rowSelection={rowSelection}
-        scroll={scroll}
-        expandable={expandable}
-        pagination={false}
-        {...props}
-      />
-      {pagination && (
-        <div className="flex justify-end mt-4">
-          <div className="mr-6">ทั้งหมด: {config.total} รายการ</div>
-          <Pagination
-            total={config.total}
-            current={config.currentPage}
-            pageSize={config.pageSize}
-            onChange={(page) => {
-              onChangePage && onChangePage(page, "page");
-            }}
-            showSizeChanger
-            onShowSizeChange={(_current, pageSize) =>
-              onChangePage && onChangePage(pageSize, "pageSize")
-            }
-          />
         </div>
-      )}
+         
+        <Table
+          columns={columns}
+          dataSource={[...dataSource]}
+          rowSelection={rowSelection}
+          scroll={scroll}
+          expandable={expandable}
+          pagination={false}
+          {...props}
+        />
+        {pagination && (
+          <div className="flex justify-end items-center mt-4">
+            <div className="mr-6">ทั้งหมด: {config.total} รายการ</div>
+            <Pagination
+              total={config.total}
+              current={config.currentPage}
+              pageSize={config.pageSize}
+              onChange={(page) => {
+                onChangePage && onChangePage(page, "page");
+              }}
+              showSizeChanger
+              onShowSizeChange={(_current, pageSize) =>
+                onChangePage && onChangePage(pageSize, "pageSize")
+              }
+            />
+          </div>
+        )}
       </Card>
     </>
   );
