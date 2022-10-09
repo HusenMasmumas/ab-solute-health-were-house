59f033df9d5e0b97a8e601161a51423e5cf6c476
Issarapap <film4777@gmail.com>
2022-10-09 12:51:16 +0700

    refactor: 💡 วางโค้ดใหม่

---
+++ src/page/report/importedReport/interface.ts
@@ -0,0 +1,12 @@
+export interface IImportTable {
+    key: React.Key;
+    date: string;
+    code: string;
+    sku: string;
+    costPrice: number;
+    nameProduct: string;
+    retailPrice: number;
+    amount: number;
+    state: string;
+}
+  
