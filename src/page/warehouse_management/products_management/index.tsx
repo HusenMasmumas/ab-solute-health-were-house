93c4b85fc823c1ec45a69bf88911921c3f39cf4f
Issarapap <film4777@gmail.com>
2022-10-09 16:39:08 +0700

    refactor: 💡 table product management

--- src/page/warehouse_management/products_management/index.tsx
+++ src/page/warehouse_management/products_management/index.tsx
@@ -1,38 +1,36 @@
-import { Image } from "antd";
+import { ColumnsType } from "antd/lib/table";
 import SearchForm, { IsearchFormItem } from "component/Form/searchForm";
 import CHeader from "component/headerPage/Header";
+import CSelectStatus from "component/input/c-select-status";
+import MoTable from "component/Table/MoTable";
+import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-import TableProductsManagement from "views/warehouse_managment/products_management/TableProductsManagement";
 import { ProductsType } from './interface'
+
+
+const expandable = {
+  expandedRowRender: (record: ProductsType) => (
+    <div className="py-[12px] px-[65px]">{record.name}</div>
+  ),
+  rowExpandable: (record: any) => record.name !== "Not Expandable",
+};
 
 const ProductsMangement = () => {
   const navigate = useNavigate();
-  // const { t } = useTranslation();
-  const data: ProductsType[] = [
-    {
-      key: 1,
-      name: "K8763",
-      sku: "โซเดียมไบคาร์บอเนต",
-      subSku: "",
-      category: "เคมีภัณฑ์",
-      subCategory: "สารละลาย",
-      priceNormal: 0,
-      priceCost: 0,
-      status: "เปิด",
-    },
-    {
-      key: 2,
-      name: "K8763",
-      sku: "โซเดียมไบคาร์บอเนต",
-      subSku: "",
-      category: "เคมีภัณฑ์",
-      subCategory: "สารละลาย",
-      priceNormal: 0,
-      priceCost: 0,
-      status: "ปิด",
-    },
-  ];
+  const [limitPage, setLimitPage] = useState<number>(10);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    console.log("current", currentPage);
+    console.log("limitPage", limitPage);
+  }, [currentPage, limitPage]);
+
+  const onChangePage = (page: number, type?: string) => {
+    if (type === "pageSize") setLimitPage(page);
+    else setCurrentPage(page);
+  };
 
   const elements: IsearchFormItem[] = [
     {
@@ -86,6 +84,44 @@ const ProductsMangement = () => {
     },
   ];
 
+  const columns: ColumnsType<ProductsType> = [
+    {
+      title: "SKU/Sub SKU",
+      dataIndex: "sku",
+      width: "20%",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      width: "20%",
+    },
+    {
+      title: "Category/Sub Category",
+      dataIndex: "category",
+      width: "50%",
+    },
+    {
+      title: "สถานะ",
+      dataIndex: "status",
+      render: (status, row) => {
+        return (
+          <CSelectStatus
+            state={status}
+            listOption={[
+              { label: "ปิด", value: "ปิด" },
+              { label: "เปิด", value: "เปิด" },
+            ]}
+            labelKey={"label"}
+            valueKey={"value"}
+            activeBackground={"#77C48B"}
+            initialValue={status}
+            activeValue={"เปิด"}
+          />
+        );
+      },
+    },
+  ];
+
   const onFinish = (values: any) => {
     //โยนเข้า create query
     console.log("Received values of form: ", values);
@@ -106,8 +142,49 @@ const ProductsMangement = () => {
         ]}
       />
       <SearchForm elements={elements} onFinish={onFinish} /> 
-      <TableProductsManagement dataTable={data} />
+      <MoTable
+        headerTable={`${t("รายการสินค้า")}`}
+        columns={columns}
+        dataSource={data}
+        expandable={expandable}
+        onChangePage={onChangePage}
+        config={{
+          total: 20, //ค่าจาก backend ใช้หารหน้า
+          pageSize: limitPage,
+          currentPage: currentPage,
+        }}
+        actions={[{
+          type: 'excel',
+          fn: ()=>{console.log('download excel');
+          }
+        }]}
+      />
     </>
   );
 };
 export default ProductsMangement;
+
+const data: ProductsType[] = [
+  {
+    key: 1,
+    name: "K8763666",
+    sku: "โซเดียมไบคาร์บอเนต",
+    subSku: "",
+    category: "เคมีภัณฑ์",
+    subCategory: "สารละลาย",
+    priceNormal: 0,
+    priceCost: 0,
+    status: "เปิด",
+  },
+  {
+    key: 2,
+    name: "K8763",
+    sku: "โซเดียมไบคาร์บอเนต",
+    subSku: "",
+    category: "เคมีภัณฑ์",
+    subCategory: "สารละลาย",
+    priceNormal: 0,
+    priceCost: 0,
+    status: "ปิด",
+  },
+];
