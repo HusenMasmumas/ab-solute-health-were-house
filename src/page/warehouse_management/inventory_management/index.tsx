3c15e215f2a9e99bc55c1ead6a0195f0e6cd1229
Issarapap <film4777@gmail.com>
2022-10-10 15:56:34 +0700

    style: 💄 hover

--- src/page/warehouse_management/inventory_management/index.tsx
+++ src/page/warehouse_management/inventory_management/index.tsx
@@ -78,7 +78,7 @@ const WarehouseManagement = () => {
               className="bg-white px-[24px] pt-[16px] py-[10px] rounded-[10px] hover:border-darkblue hover:border"
             >
               <div className="grid justify-end items-end">
-                <div className="w-[28px] h-[28px] bg-lightsky text-green rounded-[5px] grid justify-center items-center">
+                <div className="w-[28px] h-[28px] bg-lightsky text-green rounded-[5px] grid justify-center items-center hover:cursor-pointer">
                   <DotIcon
                     onClick={() => {
                       // navigate("/warehouse-management/create-inventory");
@@ -99,17 +99,17 @@ const WarehouseManagement = () => {
               <div>
                 <span className="text-lightgray !mb-0">{`${t(item.code)}`}</span>
               </div>
-              <div className="grid grid-cols-2">
+              <div className="grid grid-cols-2 content-center !place-content-between">
                 <span
-                  className="text-lightblue text-[20px] font-bold"
+                  className="text-lightblue text-[20px] font-bold flex items-center"
                   onClick={() => {
                     navigate("/warehouse-management/store-cabinet");
                   }}
                 >
                   {item.storeNo}
                 </span>
-                <div className="flex text-green  items-end justify-end" >
-                  <ArrowIcon onClick={()=>{navigate("/warehouse-management/store-cabinet", {state:{id: item.id }})}}/> 
+                <div className=" text-green  w-full flex justify-end" >
+                  <ArrowIcon className="hover:cursor-pointer w-12" onClick={()=>{navigate("/warehouse-management/store-cabinet", {state:{id: item.id }})}}/> 
                 </div>
               </div>
             </div>
