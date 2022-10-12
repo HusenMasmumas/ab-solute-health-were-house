91184efa2054eff7bb2a07244a284d55aa2099d3
Issarapap <film4777@gmail.com>
2022-10-12 11:57:28 +0700

    style: 💄 z-index

--- src/component/headerPage/Header.tsx
+++ src/component/headerPage/Header.tsx
@@ -27,7 +27,7 @@ const CHeader = ({ keyHeader, arrPath = [], buttons=[] }: ICHeader) => {
     return i18n.exists(keytext) ? `${t(keytext)}` : keytext
   }
   return (
-    <div className="grid grid-cols-2 pb-5 pt-5" style={{ position: "sticky", width: "100%", top: 0, zIndex:1, backgroundColor:'#F5F5F5'}}>
+    <div className="grid grid-cols-2 pb-5 pt-5 z-40" style={{ position: "sticky", width: "100%", top: 0, backgroundColor:'#F5F5F5'}}>
       <div className="w-full">
         <div className="text-[24px] text-darkblue font-bold">
           {i18n.exists(keyHeader) ? `${t(keyHeader)}` : keyHeader}
