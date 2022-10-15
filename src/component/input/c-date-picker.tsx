4693c88aef374b12416f913fca59e77dbc0c3f06
Issarapap <film4777@gmail.com>
2022-10-15 22:37:43 +0700

    refactor: 💡 style Cdatepicker style

--- src/component/input/c-date-picker.tsx
+++ src/component/input/c-date-picker.tsx
@@ -1,45 +1,20 @@
-import {
-    PickerProps,
-    RangePickerProps,
-  } from "antd/es/date-picker/generatePicker";
   import styled from "styled-components";
-//   import { ITheme, ThemeOptions, ThemeTypes } from "../../constrant/theme";
-  import CDate from "./c-date";
-  import { ConfigProvider } from 'antd';
-  export const StyleCDatePicker = styled(CDate)`
-  height:43px;
+  import { ConfigProvider, DatePicker, DatePickerProps } from 'antd';
+  
+  export const StyleCDatePicker = styled(DatePicker)`
+  height:45px;
   width: 100%;
+  border-radius: 5px !important;
   `;
-  
-  type CDatePickerProps = PickerProps<any> & {
-  };
-  
-  const CDatePicker = (props: CDatePickerProps) => {
-    const { ...rest } = props;
-    // const themeProps = ThemeOptions[theme];
+    
+  const CDatePicker = (props: DatePickerProps) => {
+    
     return (
       <ConfigProvider getPopupContainer={(trigger:any) => trigger.parentElement}>
-        <StyleCDatePicker  {...rest} />
+        <StyleCDatePicker  size="large"/>
       </ConfigProvider>
     )
   };
-  
-  type CRangePickerProps = RangePickerProps<any> & {
-
-  };
-  
-  const RangePicker = (props: CRangePickerProps) => {
-    const { ...rest } = props;
-    // const themeProps = ThemeOptions[theme];
-    return (
-      <StyleCDatePicker.RangePicker
-        style={{ height: 45, width: "100%" }}
-        {...rest}
-      />
-    );
-  };
-  
-  CDatePicker.RangePicker = RangePicker;
   
   export default CDatePicker;
   
