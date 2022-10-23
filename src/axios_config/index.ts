341dec5a68b83d3825c2cb00a01a612326c13c91
Issarapap <film4777@gmail.com>
2022-10-24 01:37:16 +0700

    refactor: 💡 config axios

---
+++ src/axios_config/index.ts
@@ -0,0 +1,47 @@
+import Axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+import { apiURL } from "config/api";
+
+const axios = Axios.create({
+  baseURL: apiURL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+  validateStatus: (status) => status >= 400,
+});
+
+interface AxiosCustomRequestConfig<D = any> extends AxiosRequestConfig<D> {
+  _retry: boolean;
+}
+interface AxiosCustomError<T = any, D = any> extends AxiosError<T, D> {
+  config: AxiosCustomRequestConfig<D>;
+}
+const onRequest = (config: AxiosRequestConfig): AxiosRequestConfig => {
+  const acToken = localStorage.getItem("accessToken");
+  config.validateStatus = (_) => true;
+  config.headers = {
+    Authorization: `Bearer ${acToken}`,
+  };
+  return config;
+};
+
+const onRequestError = (error: AxiosError): Promise<AxiosError> => {
+  return Promise.reject(error);
+};
+
+const onResponse = (response: AxiosResponse): AxiosResponse => {
+  return response;
+};
+
+const onResponseError = async (error: AxiosCustomError) => {
+  if (error.response) {
+    if (error.response.status === 403 && error.response.data) {
+      return Promise.resolve(error.response.data);
+    }
+    return Promise.reject(error);
+  }
+  return Promise.reject(error);
+};
+
+axios.interceptors.request.use(onRequest, onRequestError);
+
+axios.interceptors.response.use(onResponse, onResponseError);
+export default axios;
---
+++ src/config/api.ts
@@ -0,0 +1,2 @@
+export const apiURL = process.env.REACT_APP_API_URL;
+export const version = process.env.REACT_APP_API_VERSION;
