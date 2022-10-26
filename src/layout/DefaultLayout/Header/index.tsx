e7e2eb0b403ca7b05336f00b8f670fff47c76f3c
Issarapap <film4777@gmail.com>
2022-10-26 15:40:04 +0700

    refactor: 💡 remove comment

--- src/layout/DefaultLayout/Header/index.tsx
+++ src/layout/DefaultLayout/Header/index.tsx
@@ -36,7 +36,6 @@ const Hader = ({ setOpenDrawer, openDrawer }: Props) => {
     i18n.changeLanguage(lang);
   }
   return (
-    // <div className="header duration-500 bg-gradient-to-r from-[#148ce7] dark:from-[#091a36] dark:to-[#20252d] py-2 px-6  to-white min-h-[80px] border-b-4 dark:border-[#16181b] flex justify-between items-center">
     <div className="header duration-500 bg-[#01438F] dark:bg-black py-2 px-6  min-h-[90px] flex justify-between items-center">
       <div className="flex items-center gap-4">
         <div
@@ -46,11 +45,7 @@ const Hader = ({ setOpenDrawer, openDrawer }: Props) => {
             setOpenDrawer(!openDrawer);
           }}
         >
-          {/* <MinusIcon className="w-full h-full my-auto text-secondary-default cursor-pointer dark:fill-white " /> */}
         </div>
-        {/* <div className="text-[#123265] dark:text-white text-base xl:text-lg 2xl:text-[28px] flex">
-          // <span className="font-bold  ">Template </span> 
-        </div> */}
       </div>
       <div className="flex items-center">
         <div className="h-14 flex items-center ">
@@ -69,34 +64,6 @@ const Hader = ({ setOpenDrawer, openDrawer }: Props) => {
             </Option>
           </Select>
         </div>
-
-        {/* <div className=" gap-1 px-2 py-1 flex justify-center items-center rounded-full bg-[#F2F8FF] dark:bg-[#16181c] duration-500 ">
-          <div
-            className=" rounded-full flex justify-center items-center bg-white dark:bg-[#16181c] duration-500 w-8 h-8 cursor-pointer"
-            onClick={() => {
-              setTheme.SetLight();
-            }}
-          >
-            {getTheme() === "light" ? (
-              <Sun className=" object-cover p-2 fill-white stroke-primary-default stroke-[1px]" />
-            ) : (
-              <Light className=" object-cover p-2 fill-white stroke-white stroke-[1px]" />
-            )}
-          </div>
-          <div
-            className=" rounded-full flex justify-center items-center w-8 h-8 hover:bg-white dark:bg-[#20252d] duration-500 group cursor-pointer"
-            onClick={() => {
-              setTheme.SetDark();
-            }}
-          >
-            {getTheme() === "dark" ? (
-              <Dark className=" object-cover p-2 fill-white stroke-primary-default stroke-[1px]" />
-            ) : (
-              <Moon className=" object-cover p-2 fill-white stroke-primary-default stroke-[1px]" />
-            )}
-          </div>
-        </div> */}
-
         <div className="max-h-12 w-12 ml-4 bg-[#F2F8FF] rounded-full border-2 border-[#3B8DE2]">
           <Image
             src={Profile}
