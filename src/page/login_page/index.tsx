119b85608438d877ecceffa14727f926e2f03f39
Issarapap <film4777@gmail.com>
2022-10-27 13:06:43 +0700

    style: 💄 layout style login page

--- src/page/login_page/index.tsx
+++ src/page/login_page/index.tsx
@@ -1,62 +1,110 @@
-import React, { useState } from "react";
-import { Input, Checkbox, Button } from "antd";
-import style from "./style.module.css";
-import clsx from "clsx";
+import { Checkbox, Image, Form } from "antd";
 import "../../styles/tailwind.css";
 import "./style.css";
 import { useNavigate } from "react-router-dom";
 import { useAuthContextDispatch } from "context/Auth/store";
+import LoginImg from "assets/img/login.jpg";
+import Logo from "assets/img/logo.png";
+import { KeyOutlined, UserOutlined } from "@ant-design/icons";
+import CInput from "component/input/c-input";
+import DeepBlueButton from "component/Button/DeepBlue";
+import { useLogin } from "service/auth";
 const LoginPage = () => {
+  const [form] = Form.useForm();
   const navigate = useNavigate();
   const { _signIn } = useAuthContextDispatch();
+  const uselogin = useLogin();
+  const onFinish = (values: any) => {
+    uselogin.mutate(values, {
+      onSuccess: (res) => {
+        console.log("sucess", res);
+        _signIn({
+          token: { accessToken: res.accessToken },
+          userInfo: { id: "", email: "", role: res.role },
+        });
+        navigate("/", { replace: true });
+      },
+      onError: (error: any) => {
+        alert(error);
+      },
+    });
+  };
   return (
-    <div className="min-w-screen min-h-screen grid grid-cols-3 !text-white">
-      <div className="col-span-2 w-full h-full relative bg-secondary flex flex-col justify-center items-center">
-        <h1 className="text-white text-[50px] font-bold">
-          Starter Template Backoffice{" "}
-        </h1>
-        <h1 className="text-white text-[50px] font-bold"> With TailwindCSS </h1>
-        <h2 className="text-white text-[20px] absolute bottom-6 left-6 ">
-          {" "}
-          Create by Pat
-        </h2>
+    <div className="min-w-screen min-h-screen grid grid-cols-7 !text-white">
+      <div className="col-span-4">
+        <Image
+          src={LoginImg}
+          alt="login.jpg"
+          preview={false}
+          style={{ height: "100vh" }}
+        />
       </div>
-
-      <div className="w-full h-full bg-[#123265] flex flex-col gap-6 items-center justify-center  p-[6rem]">
-        <h1 className="text-white !font-semibold !text-3xl">Login Account</h1>
-        <Input
-          className="w-full !mt-12 !h-12 !p-4 !border-l-[6px] !border-[#2053a4] !text-xl text-black"
-          placeholder="Email ID"
-          type={"email"}
+      <div className="w-full h-full bg-slate-0 flex flex-col gap-6 items-center justify-center  p-[6rem] col-span-3">
+        <Image
+          className="!h-[200px] !bg-[#FFF] flex justify-center items-center px-[40px]"
+          src={Logo}
+          preview={false}
         />
-        <Input
-          className="w-full mt-6 !h-12 !p-4 !border-l-[6px] !border-[#2053a4] !text-xl text-black"
-          placeholder="Password"
-          type={"password"}
-        />
-        <div className="flex justify-between mt-6 !text-lg  w-full">
-          <Checkbox
-            className={clsx(
-              "w-full !text-lg !sr-onlyfont-medium  !pb-4 border-l-6 border-[#2053a4] !text-white flex items-start check_box",
-              style.check_box
-            )}
-          >
-            <span className="opacity-50">Keep me signed in</span>
-          </Checkbox>
-          <p className="text-white w-full opacity-50 flex justify-end items-center">
-            Already a member?
-          </p>
-        </div>
-        <Button
-          className="w-full !mt-6 !bg-[#2053a4] !border-[#2053a4] !tracking-wider !uppercase !text-xl !py-8 !p-4 !rounded-full text-white !flex !justify-center !items-center"
-          type="primary"
-          onClick={() => {
-            console.log("sign in");
-            navigate("/");
-          }}
+        <h1 className="text-[#949594] !font-semibold !text-[20px]">
+          Login in your account to continue.
+        </h1>
+        <Form
+          name="basic"
+          form={form}
+          onFinish={onFinish}
+          // onFinishFailed={onFinishFailed}
+          layout="vertical"
+          autoComplete="off"
+          className="w-4/5"
+          requiredMark={false}
         >
-          Login
-        </Button>
+          <Form.Item
+            label={
+              <div className="!text-[20px] text-[#01438F] font-semibold ">
+                ชื่อผู้ใช้
+              </div>
+            }
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <CInput
+              className="w-full !h-12 !p-4 !border-l-[6px] !border-[#2053a4] !text-xl text-black"
+              placeholder="ชื่อผู้ใช้"
+              prefix={<UserOutlined />}
+            />
+          </Form.Item>
+          <Form.Item
+            label={
+              <div className="!text-[20px] text-[#01438F] font-semibold">
+                รหัส
+              </div>
+            }
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <CInput
+              className="w-full !h-12 !p-4 !border-l-[6px] !border-[#2053a4] !text-xl text-black"
+              placeholder="Password"
+              type={"password"}
+              prefix={<KeyOutlined />}
+            />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox>
+              จำรหัสผ่าน
+              {/* <span className="text-[#949594]">จำรหัสผ่าน</span> */}
+            </Checkbox>
+          </Form.Item>
+          <DeepBlueButton
+            className="!w-full"
+            type="primary"
+            onClick={() => {
+              form.submit();
+            }}
+          >
+            เข้าสู่ระบบ
+          </DeepBlueButton>
+        </Form>
       </div>
     </div>
   );
