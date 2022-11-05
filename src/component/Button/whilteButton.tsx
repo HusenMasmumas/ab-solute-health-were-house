ecabb9593315a44f439a2fab9e9ccc369dd42e75
Issarapap <film4777@gmail.com>
2022-11-05 17:48:53 +0700

    refactor: 💡 close warning

--- src/component/Button/whilteButton.tsx
+++ src/component/Button/whilteButton.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 interface Props extends BaseButtonProps {}
 
-const WhilteButton = styled(BaseButton)<BaseButtonProps>`
+const WhilteButton = styled(BaseButton)<Props>`
   color: #4D4D4D;
   background-color: #FFF;
   ${({ fontSize }) => ( fontSize ? `font-size: ${fontSize};` : null)}}
