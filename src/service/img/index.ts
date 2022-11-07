6a8bde5e1a2ab093cd4928df090830a4312d8d5b
Issarapap <film4777@gmail.com>
2022-11-07 16:51:22 +0700

    refactor: 💡 remove not use

--- src/service/img/index.ts
+++ src/service/img/index.ts
@@ -1,7 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import axios, { throwResponse } from 'axios_config'
 
-// axios.defaults.headers.common['Content-Type'] = 'multipart/form-data'
 
 export const useCreatImg = () => {
     return useMutation(async (data: any) => {
