52fb2b0b796ea3f9c43cb2fe8f10936d3de4d458
Issarapap <film4777@gmail.com>
2022-11-14 17:36:19 +0700

    refactor: 💡 unused code

--- src/component/Form/createForm.tsx
+++ src/component/Form/createForm.tsx
@@ -1,4 +1,4 @@
-import { Col, Form, FormInstance, Input, Row } from "antd";
+import { Col, Form, FormInstance, Row } from "antd";
 import { Store } from "antd/lib/form/interface";
 import CDatePicker from "component/input/c-date-picker";
 import CInput from "component/input/c-input";
@@ -34,49 +34,8 @@ const CreateForm = ({
     }
   };
 
-  const _onReset = () => {
-    if (form) {
-      form.resetFields();
-      return form.submit();
-    }
-    _form.resetFields();
-    _form.submit();
-  };
-
-  const _onSubmit = () => {
-    // ทำงานเหมือนกดปุ่ม เรียกปุ่ม submit buttom
-    if (form) {
-      return form.submit();
-    }
-    _form.submit();
-  };
-
-  const getPlaceholder = (
-    input: TPageHeaderInput,
-    name: string
-  ): string | undefined => {
-    const list: TPageHeaderInput[] = [
-      "input",
-      "select",
-      "select-car-brand",
-      "select-car-type",
-      "select-color",
-      "select-pagination",
-    ];
-    if (list.includes(input)) {
-      return name;
-    }
-  };
-
-  const getOnEnter = (input: TPageHeaderInput): object => {
-    if (input === "input") {
-      return { onPressEnter: _onSubmit };
-    }
-    return {};
-  };
   return (
-    <div>
-      <div>
+      <>
         <Form
           layout="vertical"
           onFinish={onFinish}
@@ -86,8 +45,6 @@ const CreateForm = ({
           <Row gutter={[12, 0]}>
             {elements.map((item) => {
               const Element = getInputByType(item.input.type) as ElementType;
-              const onEnter = getOnEnter(item.input.type);
-              const placeholder = getPlaceholder(item.input.type, item.label);
               return (
                 <Col span={12} key={item.name}>
                   <Form.Item
@@ -107,8 +64,7 @@ const CreateForm = ({
             })}
           </Row>
         </Form>
-      </div>
-    </div>
+      </>
   );
 };
 export default CreateForm;
